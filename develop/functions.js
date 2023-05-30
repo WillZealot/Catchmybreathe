a1337d8735c2b87e61cbd50aa04c228828e6a7ea
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-30 18:35:25 -0400

    addded fethes to geocoding city name inputs and a fetch for the cities pollution utilizing the geocoding api its saving the inputed city once but whenever a new one is searched it changes it so we have to fix that

--- develop/functions.js
+++ develop/functions.js
@@ -1,27 +1,6 @@
 let KeyApi = "b063e961132d34721eb67544bf97f624"
 
-let pollutionUrl = "http://api.openweathermap.org/data/2.5/air_pollution?lat=28.538336&lon=-81.379234&appid="+ KeyApi;
-
-fetch(pollutionUrl)
-.then(function(response){
-    return response.json();
-})
-
-.then(function(data){
-    console.log(data);
-})
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior : "smooth"
-        });
-    });
-});
-
-
-//Directs search to other pages according to set parameters.
+  //Directs search to other pages according to set parameters.
 
   function performSearch() {
     const progressBar = document.querySelector(".progress");
@@ -41,23 +20,58 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
     }, 20);
   }
   
-
-  const savedSearchQuery = localStorage.getItem("searchQuery");
-  // Saves the search to local storage
-  function saveSearch(searchQuery) {
-    if (typeof Storage !== "undefined") {
-      localStorage.setItem("searchQuery", searchQuery);
-    }
-  }
-  
-
   //searchButton and "Enter" key actions 
   const searchButton = document.querySelector("#userCityButton");
   const userCityInput = document.getElementById("userCityInput");
-  
-  searchButton.addEventListener("click", performSearch);
-  userCityInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      performSearch();
-    }
+
+  searchButton.addEventListener("click", function(){
+    //performSearch();
+    let cityInput = $("input").val();
+    console.log(cityInput);
+    localStorage.setItem("searchQuery", cityInput);
+    
+    let letCityInfo = "https://api.openweathermap.org/geo/1.0/direct?q="+ cityInput +"&limit=1&appid=" + KeyApi;
+
+        fetch(letCityInfo)
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(data){
+        console.log(data);
+        
+        let searchLatEl = data[0].lat;
+        let searchLonEl = data[0].lon;
+
+        let pollutionUrl = "https://api.openweathermap.org/data/2.5/air_pollution?lat="+ searchLatEl +"&lon="+ searchLonEl +"&appid="+ KeyApi;
+
+        fetch(pollutionUrl)
+        .then(function(response){
+            return response.json();
+        })
+    
+        .then(function(data){
+            console.log(data);
+        })
   });
+})
+
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
+
+
+  
+
+
+  
+
