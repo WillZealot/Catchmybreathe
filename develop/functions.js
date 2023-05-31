4fde42a9b853de13cdc7a4b24b32441969697839
Noah Nelson <noah.james.nelson@gmail.com>
2023-05-31 19:33:13 -0400

    Added navbar responsiveness and js functionality

--- develop/functions.js
+++ develop/functions.js
@@ -75,3 +75,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
 
   
 
+// mobile menu
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
