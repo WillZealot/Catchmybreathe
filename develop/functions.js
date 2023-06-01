260f3f8f6ab2b4d5f913d2aa13e2c50534c4789a
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-06-01 18:55:15 -0400

    Local storage conversion to an Array and saved upon submition to the next page document html for functionality

--- develop/functions.js
+++ develop/functions.js
@@ -1,87 +1,112 @@
-let KeyApi = "b063e961132d34721eb67544bf97f624"
+let KeyApi = "b063e961132d34721eb67544bf97f624";
 
-  //Directs search to other pages according to set parameters.
+// Define the saveSearch function
+function saveSearch(cityInput, geocodingData) {
+  // Retrieve the existing searchQueries array from localStorage or create a new empty array
+  let searchQueries = JSON.parse(localStorage.getItem("searchQueries")) || [];
 
-  function performSearch() {
-    let progressBar = document.querySelector(".progress");
-    progressBar.value = 0;
-    let interval = setInterval(function() {
-      progressBar.value += 1;
-      if (progressBar.value === progressBar.max) {
-        clearInterval(interval);
-        let searchQuery = userCityInput.value.trim();
-        if (searchQuery === "") {
-          window.location.href = "./404.html";
-        } else {
-          window.location.href = "./results.html";
-        }
-      }
-    }, 20);
+  // Add the current search query to the array if it doesn't already exist
+  if (!searchQueries.includes(cityInput)) {
+    searchQueries.push(cityInput);
   }
-  
-  //searchButton and "Enter" key actions 
-let searchButton = $("#userCityButton");
-let userCityInput = $("#userCityInput");
-searchButton.on("click", function() {
- performSearch();
- let cityInput = $("input").val();
- console.log(cityInput);
- // Retrieve the existing search queries from local storage
- let searchQueries = localStorage.getItem("searchQueries");
- if (searchQueries) {
-  // If searchQueries already exists in local storage, parse it from a string to an array
-  searchQueries = JSON.parse(searchQueries);
- } else {
-  // If searchQueries does not exist in local storage, initialize it as an empty array
-  searchQueries = [];
- }
- // Add the current search query to the array
- searchQueries.push(cityInput);
- // Save the updated search queries array back to local storage
- localStorage.setItem("searchQueries", JSON.stringify(searchQueries));
- let cityInfoUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + KeyApi;
- fetch(cityInfoUrl)
-  .then(function(response) {
-   return response.json();
-  })
-  .then(function(data) {
-   console.log(data);
-   let searchLatEl = data[0].lat;
-   let searchLonEl = data[0].lon;
-   let pollutionUrl = "https://api.openweathermap.org/data/2.5/air_pollution?lat=" + searchLatEl + "&lon=" + searchLonEl + "&appid=" + KeyApi;
-   fetch(pollutionUrl)
-    .then(function(response) {
-     return response.json();
-    })
-    .then(function(data) {
-     console.log(data);
+
+  // Save the updated array back to localStorage
+  localStorage.setItem("searchQueries", JSON.stringify(searchQueries));
+
+  // Save the geocoding data for the current search query
+  localStorage.setItem(cityInput, JSON.stringify(geocodingData));
+}
+
+// Directs search to other pages according to set parameters.
+function performSearch() {
+  const progressBar = document.querySelector(".progress");
+  progressBar.value = 0;
+  const interval = setInterval(function() {
+    progressBar.value += 1;
+    if (progressBar.value === progressBar.max) {
+      clearInterval(interval);
+      const searchQuery = userCityInput.value.trim();
+      if (searchQuery === "") {
+        window.location.href = "./404.html";
+      } else {
+        letCityInfo =
+          "https://api.openweathermap.org/geo/1.0/direct?q=" +
+          searchQuery +
+          "&limit=1&appid=" +
+          KeyApi;
+
+        fetch(letCityInfo)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            console.log(data);
+
+            let searchLatEl = data[0].lat;
+            let searchLonEl = data[0].lon;
+
+            let pollutionUrl =
+              "https://api.openweathermap.org/data/2.5/air_pollution?lat=" +
+              searchLatEl +
+              "&lon=" +
+              searchLonEl +
+              "&appid=" +
+              KeyApi;
+
+            fetch(pollutionUrl)
+              .then(function(response) {
+                return response.json();
+              })
+              .then(function(pollutionData) {
+                console.log(pollutionData);
+
+                // Save the geocoding data and perform the redirection
+                saveSearch(searchQuery, {
+                  lat: searchLatEl,
+                  lon: searchLonEl,
+                  pollutionData: pollutionData
+                });
+
+                window.location.href = "./results.html";
+              });
+          });
+      }
+    }
+  }, 20);
+}
+
+// Check if the DOM content is loaded before accessing the elements
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the search button element
+  const searchButton = document.querySelector("#userCityButton");
+  const userCityInput = document.getElementById("userCityInput");
+
+  // Check if the search button element exists
+  if (searchButton) {
+    // Add click event listener to the search button
+    searchButton.addEventListener("click", function() {
+      performSearch();
+      let cityInput = $("input").val();
+      console.log(cityInput);
+    });
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
     });
   });
-  
 });
 
+// Mobile menu toggle
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
 
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior : "smooth"
-        });
-    });
-});
-
-
-
-  
-
-
-  
-
-// mobile menu
-let burgerIcon = $('#burger');
-let navbarMenu = $('#nav-links');
-
-burgerIcon.on('click', () => {
-  navbarMenu.classList.toggle('is-active');
-});
+if (burgerIcon && navbarMenu) {
+  burgerIcon.addEventListener('click', function() {
+    navbarMenu.classList.toggle('is-active');
+  });
+}
