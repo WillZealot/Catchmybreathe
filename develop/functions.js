aec25d5ec5445536b99b8c1b082f0b077173de13
william <williamtikhonenko@gmail.com>
2023-06-04 19:45:14 -0400

    emoji indicates the quality of the air

--- develop/functions.js
+++ develop/functions.js
@@ -1,7 +1,4 @@
 let KeyApi = "b063e961132d34721eb67544bf97f624";
-
-
-let emojiResult = document.querySelector("#emojiConditions");
 // Define the saveSearch function
 function saveSearch(cityInput, geocodingData) {
   let searchQueries = JSON.parse(localStorage.getItem("searchQueries")) || [];
@@ -13,58 +10,48 @@ function saveSearch(cityInput, geocodingData) {
   localStorage.setItem("searchQueries", JSON.stringify(searchQueries));
   localStorage.setItem(cityInput, JSON.stringify(geocodingData));
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  //let airQElement = document.getElementById("airQ");
-  //let no2Level = JSON.stringify(localStorage.getItem("pollutionData"));
-  //airQElement.textContent = "NO2 Level: " + no2Level;
-
+document.addEventListener("DOMContentLoaded", function () {
   // Display the first index of searchQueries on the card
   const searchedCityUser = document.getElementById("searchedCityUser");
   const searchQueries = JSON.parse(localStorage.getItem("searchQueries"));
   if (searchedCityUser && searchQueries && searchQueries.length > 0) {
     const firstSearchQuery = searchQueries[0];
-    const capitalizedCity = firstSearchQuery.charAt(0).toUpperCase() + firstSearchQuery.slice(1);
+    const capitalizedCity =
+      firstSearchQuery.charAt(0).toUpperCase() + firstSearchQuery.slice(1);
     searchedCityUser.textContent = capitalizedCity;
+    setAirDataEmoji(capitalizedCity);
   }
-
   const searchButton = document.querySelector("#userCityButton");
   // Check if the search button element exists
   if (searchButton) {
-    searchButton.addEventListener("click", function() {
+    searchButton.addEventListener("click", function () {
       let cityInput = document.querySelector("input").value;
       console.log(cityInput);
       performSearch(cityInput);
     });
   }
-
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
       e.preventDefault();
       document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth"
+        behavior: "smooth",
       });
     });
   });
-
   // Mobile menu toggle
-  const burgerIcon = document.querySelector('#burger');
-  const navbarMenu = document.querySelector('#nav-links');
+  const burgerIcon = document.querySelector("#burger");
+  const navbarMenu = document.querySelector("#nav-links");
   if (burgerIcon && navbarMenu) {
-    burgerIcon.addEventListener('click', function() {
-      navbarMenu.classList.toggle('is-active');
+    burgerIcon.addEventListener("click", function () {
+      navbarMenu.classList.toggle("is-active");
     });
   }
-
   function performSearch(cityInput) {
     const progressBar = document.querySelector(".progress");
     progressBar.value = 0;
     progressBar.max = 100;
-
-    const interval = setInterval(function() {
+    const interval = setInterval(function () {
       progressBar.value += 1;
-
       if (progressBar.value === progressBar.max) {
         clearInterval(interval);
         if (cityInput === "") {
@@ -72,52 +59,74 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
           let capitalizedSearchQuery = cityInput
             .split(" ")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
             .join(" ");
-
-          let letCityInfo = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + KeyApi;
-
+            let letCityInfo =
+            "https://api.openweathermap.org/geo/1.0/direct?q=" +
+            cityInput +
+            "&limit=1&appid=" +
+            KeyApi;
           fetch(letCityInfo)
-            .then(function(response) {
+            .then(function (response) {
               return response.json();
             })
-            .then(function(data) {
+            .then(function (data) {
               console.log(data);
-
               let searchLatEl = data[0].lat;
               let searchLonEl = data[0].lon;
-
-              let pollutionUrl = "https://api.openweathermap.org/data/2.5/air_pollution?lat=" + searchLatEl + "&lon=" + searchLonEl + "&appid=" + KeyApi;
-
+              let pollutionUrl =
+                "https://api.openweathermap.org/data/2.5/air_pollution?lat=" +
+                searchLatEl +
+                "&lon=" +
+                searchLonEl +
+                "&appid=" +
+                KeyApi;
               fetch(pollutionUrl)
-                .then(function(response) {
+                .then(function (response) {
                   return response.json();
                 })
-                .then(function(pollutionData) {
+                .then(function (pollutionData) {
                   console.log(pollutionData);
-                  let pollutionEle = pollutionData.list[0].components.co;
+                  let pollutionEle = pollutionData.list[0].components.o3;
                   console.log(pollutionEle);
-                  let searchQueries = JSON.parse(localStorage.getItem("searchQueries")) || [];
+                  let searchQueries =
+                    JSON.parse(localStorage.getItem("searchQueries")) || [];
                   searchQueries.unshift(capitalizedSearchQuery);
                   searchQueries = searchQueries.slice(0, 5); // Limit the number of stored queries
-
-                  localStorage.setItem("searchQueries", JSON.stringify(searchQueries));
-
+                  localStorage.setItem(
+                    "searchQueries",
+                    JSON.stringify(searchQueries)
+                  );
                   // Save the geocoding data and perform the redirection to results.html
                   saveSearch(capitalizedSearchQuery, {
                     lat: searchLatEl,
                     lon: searchLonEl,
-                    pollutionData: pollutionData
+                    pollutionData: pollutionData,
                   });
-
                   // Redirect to results.html
                   window.location.href = "./results.html";
+                  getAirData();
                 });
             });
         }
       }
     }, 20);
   }
-
+  function setAirDataEmoji(city) {
+    console.log(city);
+    let weatherData = JSON.parse(localStorage.getItem(city));
+    console.log(weatherData);
+    let pollutionData = weatherData.pollutionData;
+    console.log(pollutionData);
+    let pollutionEle = pollutionData.list[0].components.o3;
+    console.log(pollutionEle);
+    let emojiResult = document.querySelector("#conditionsEmoji");
+    if (pollutionEle < 70) {
+      emojiResult.setAttribute("src", "./develop/images/Good Conditions.png");
+    } else if (pollutionEle > 125) {
+      emojiResult.setAttribute("src", "./develop/images/Bad Conditions.png");
+    } else {
+      emojiResult.setAttribute("src", "./develop/images/Ok Conditions.png");;
+    }
+  }
 });
-
